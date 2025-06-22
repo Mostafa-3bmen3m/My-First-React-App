95ea99dd8ff054d3eeb9ebb6a02d7fbaeb48d19a
Mostafa <mostafaakm531@gmail.com>
2025-06-22 23:35:41 +0300

    Configure GitHub Actions for deployment to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  base : "/My-First-React-App"
+  base : "/My-First-React-App/"
 })
